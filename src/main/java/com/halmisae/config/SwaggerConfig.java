9b440c2bfce6060a317ffa44d88532a556263f59
bbman9900 <tmddn1679@naver.com>
2024-07-23 21:23:09 +0900

    JWT Login impleãmenting

--- src/main/java/com/halmisae/config/SwaggerConfig.java
+++ src/main/java/com/halmisae/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.halmisae;
+package com.halmisae.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
--- src/main/java/com/halmisae/config/WebMvcConfig.java
+++ src/main/java/com/halmisae/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.halmisae;
+package com.halmisae.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
