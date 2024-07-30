8b2b4b7450a1540a61d7a2391acb918a897eb6a6
bbman9900 <tmddn1679@naver.com>
2024-07-30 11:07:41 +0900

    CI/CD : test-db test

--- src/test/java/com/halmisae/HalmisaeApplicationTests.java
+++ src/test/java/com/halmisae/HalmisaeApplicationTests.java
@@ -2,8 +2,10 @@ package com.halmisae;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // test 프로파일 활성화
 class HalmisaeApplicationTests {
 
     @Test
